25375fead533dde6eddc57ed5d3376d5ba084396
pvhawk <68270904+pvhawk@users.noreply.github.com>
2022-06-02 15:11:04 +0600

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -23,6 +23,20 @@ JOIN artistsalbums aa ON a.id = aa.album_id
 JOIN artists ON aa.artist_id = artists.id
 WHERE artists.name = 'Ария'
 
+SELECT a.name  FROM albums a
+JOIN artistsalbums a2 ON a2.album_id = a.id 
+JOIN artists a3 ON a2.artist_id = a3.id 
+JOIN artistsgenres ag ON ag.artist_id = a3.id 
+GROUP BY a.name
+HAVING COUNT(ag.artist_id) > 1
 
+SELECT name  FROM tracks t 
+LEFT JOIN collectionstracks c ON t.id= c.track_id WHERE c.track_id IS NULL
+
+SELECT a.name, time FROM artists a 
+JOIN artistsalbums aa ON a.id = aa.artist_id 
+JOIN albums a2 ON  a2.id = aa.album_id 
+JOIN tracks t ON t.album_id  = a2.id  
+WHERE time = (SELECT MIN(time) FROM tracks)
 
 
